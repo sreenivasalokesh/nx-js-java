05a726064b87b6cbeb1030828d5fde5cedc86f0b
slokesh <sreenivasa.lokesh@gmail.com>
2023-07-01 22:58:07 +0530

    test ci commit

--- packages/client/src/welcome/Serverwelcome.tsx
+++ packages/client/src/welcome/Serverwelcome.tsx
@@ -5,7 +5,7 @@ const ServerWelcome = () => {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    console.log('test 886');
+    console.log('test 8886');
     async function getServerData() {
       const response = await axios.get(
         `http://${process.env.NX_SERVER_URL}:${process.env.NX_SERVER_PORT}/welcome`
